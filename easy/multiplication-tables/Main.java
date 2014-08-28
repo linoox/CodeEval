79c3f5b2c29774b990f3bd9b78467dbc27fe7a8a
linoox <pd2614g@gmail.com>
2014-08-28 00:46:38 -0400

    multiplication tables ac

--- easy/multiplication-tables/Main.java
+++ easy/multiplication-tables/Main.java
@@ -12,11 +12,9 @@ public class Main {
         for (int i=1;i<=12;i++) {
             resultBuf.setLength(0);
             for (int j=1;j<=12;j++) {
-                if(i>9 && j<3)
-                    resultBuf.append(String.format("%3d",i*j));
-                else{
-                    resultBuf.append(String.format("%4d",i*j));
-                }
+                
+                resultBuf.append(String.format("%4d",i*j));
+                
             }
             row = resultBuf.toString();
             System.out.println(row.trim());
