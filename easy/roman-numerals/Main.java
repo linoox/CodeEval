a8ba8cedb21263e687786b93cd3ce80247ac6539
linoox <pd2614g@gmail.com>
2014-09-09 23:28:40 -0400

    roman numerals

--- easy/roman-numerals/Main.java
+++ easy/roman-numerals/Main.java
@@ -7,10 +7,27 @@ public class Main {
 
     static String cardinalToRoman(int number) {
 
-        Map<Integer,String> romanNumset = new HashMap<Integer, String>();
-        romanNumset.put(1,)
+        String[] romanNumerals = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        int[] cardinalNumbers = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        String romanNumber = "";
+        int power = 1;
 
+        while(number != 0) {
+            
+            for (int i=cardinalNumbers.length-1;i>=0;) {
+            
+                if(number < cardinalNumbers[i]) {
+                    i--;
 
+                } else {
+                    number = number - cardinalNumbers[i];
+                    romanNumber += romanNumerals[i];
+                }    
+            }
+
+        }
+
+        return romanNumber;
     }
 
 	public static void main (String[] args) {
