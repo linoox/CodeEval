559747f726c673ef0370835c2b63b57162e676fa
linoox <pd2614g@gmail.com>
2014-09-13 01:54:13 -0400

    counting primes

--- moderate/counting-primes/Main.java
+++ moderate/counting-primes/Main.java
@@ -7,53 +7,52 @@ import java.util.Arrays;
 
 public class Main {
 
-    // TODO: improve. naive impl    
-    public static void printPrimeNumbersUpto(int end) {
+    // TODO: improve. naive impl
+    public static void countPrimesBetween(int start, int end) {
         
-        int[] primearray= new int[end];
+        int[] primearray= new int[end+1];
         int prime=2;
         Arrays.fill(primearray, 1);
+        int primeCount=0;
         
         primearray[0]=0;
         primearray[1]=0;
         
-        for(int i=1;i<end;i++) {
+        for(int i=1;i<=end;i++) {
             
             if(primearray[i] != 0) {
                 prime = i;
-                
-                if(i > 2)
-                  System.out.print(",");
 
-                System.out.print(prime);
+                if(i>=start && i<=end)
+                    primeCount++;
                 
-                for(int j=prime; j*prime <end; j++) {
+                for(int j=prime; j*prime <=end; j++) {
                     primearray[prime*j]=0;
                 }
             }
         }
         
+        System.out.println(primeCount);
         
     }
 
 
-	public static void main (String[] args) {
+    public static void main (String[] args) {
 
         try {
             File file = new File(args[0]);
             BufferedReader in = new BufferedReader(new FileReader(file));
             String line;
             while ((line = in.readLine()) != null) {
-                
+                String[] lineArray = line.split(",");
                 if (line.length() > 0) {
-                    //System.out.println("limit:"+Integer.parseInt(line));
-                    printPrimeNumbersUpto(Integer.parseInt(line));
-                    System.out.println();
+                    
+                    countPrimesBetween(Integer.parseInt(lineArray[0]),Integer.parseInt(lineArray[1]));
                     
                 }
             }
         } catch (IOException e) {
             System.out.println("File Read Error: " + e.getMessage());
         }
-	}
+    }
 }
