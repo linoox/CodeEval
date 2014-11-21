14a1e9946f1f3ba5be18803054e3cf0408ed060f
linoox <pd2614g@gmail.com>
2014-11-20 23:06:56 -0500

    overlapping rectangles

--- moderate/overlapping-rectangles/Main.java
+++ moderate/overlapping-rectangles/Main.java
@@ -5,6 +5,27 @@ import java.io.IOException;
 
 public class Main {
 
+
+    static boolean overlappingRectangles(int[] points) {
+        int rectA_UL_x = points[0];
+        int rectA_UL_y = points[1];
+        int rectA_LR_x = points[2];
+        int rectA_LR_y = points[3];
+
+        int rectB_UL_x = points[4];
+        int rectB_UL_y = points[5];
+        int rectB_LR_x = points[6];
+        int rectB_LR_y = points[7];
+
+        if(rectA_UL_x > rectB_LR_x || rectB_UL_x > rectA_LR_x)
+            return false;
+
+        if(rectA_UL_y < rectB_LR_y || rectB_UL_y < rectA_UL_y)
+            return false;
+
+        return true;
+    }
+
 	public static void main (String[] args) {
 
         try {
@@ -12,9 +33,21 @@ public class Main {
             BufferedReader in = new BufferedReader(new FileReader(file));
             String line;
             while ((line = in.readLine()) != null) {
-                String[] lineArray = line.split("\\s");
+                String[] lineArray = line.split(",");
+                int[] points = new int[8];
+
+                for (int i=0;i<lineArray.length;i++) {
+                    points[i] = Integer.parseInt(lineArray[i]);
+                }
+
+
                 if (lineArray.length > 0) {
 
+                    if(overlappingRectangles(points)) {
+                        System.out.println("True");
+                    } else {
+                        System.out.println("False");
+                    }
                     
                 }
             }
