c94689f5cfc5afee9f125de3bb11c80a89892f91
linoox <pd2614g@gmail.com>
2014-11-21 19:49:23 -0500

    overlapping rec corrected

--- moderate/overlapping-rectangles/Main.java
+++ moderate/overlapping-rectangles/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class Main {
 
-    // todo: only 85% cases passed.
+    
     static boolean overlappingRectangles(int[] points) {
         int rectA_UL_x = points[0];
         int rectA_UL_y = points[1];
@@ -17,10 +17,8 @@ public class Main {
         int rectB_LR_x = points[6];
         int rectB_LR_y = points[7];
 
-        if(rectA_UL_x > rectB_LR_x || rectB_UL_x > rectA_LR_x)
-            return false;
-
-        if(rectA_UL_y < rectB_LR_y || rectB_UL_y < rectA_UL_y)
+        if(rectA_UL_x > rectB_LR_x || rectA_LR_x < rectB_UL_x
+            || rectA_UL_y < rectB_LR_y || rectA_LR_y > rectB_UL_y)
             return false;
 
         return true;
