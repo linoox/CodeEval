7d0efa76fe6ab4495ed3246026f9896cde78d7e2
linoox <pd2614g@gmail.com>
2015-03-27 21:48:14 -0400

    lowest common ancestor

--- moderate/lowest-common-ancestor/Main.java
+++ moderate/lowest-common-ancestor/Main.java
@@ -3,9 +3,69 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import java.util.Arrays;
+
 public class Main {
 
-	public static void main (String[] args) {
+    private Node root;
+
+    private static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int newData) {
+            left = null;
+            right = null;
+            data = newData;
+        }
+    }
+
+    
+
+    public static int lowestCommonAncestor(Node node, int key1, int key2) {            
+
+        int lca = -1;
+        
+        if (node == null) {
+            return lca;
+        }
+
+        while (node != null) {
+
+            if (node.data > key1 && node.data > key2) {
+                node = node.left;
+            } else if (node.data < key1 && node.data < key2) {
+                node = node.right;
+            } else if (node.data == key1 || node.data == key2){
+                return node.data;
+            } else {
+                return node.data;
+            }
+        }
+        
+        return lca;
+        
+    }
+
+
+    public static void main (String[] args) {        
+
+        Node root = new Node(30);
+        Node n1 = new Node(8);
+        Node n2 = new Node(52); 
+        Node n3 = new Node(3);
+        Node n4 = new Node(20);
+        Node n5 = new Node(10);
+        Node n6 = new Node(29);
+            
+        root.left = n1;
+        root.right = n2;
+        n1.left = n3;
+        n1.right = n4;
+        n4.left = n5;
+        n4.right=n6;
+    
 
         try {
             File file = new File(args[0]);
@@ -13,14 +73,14 @@ public class Main {
             String line;
             while ((line = in.readLine()) != null) {
                 String[] lineArray = line.split("\\s");
-                if (lineArray.length > 0) {
-
+                if (line.length() > 0) {
+                    
+                    System.out.println(lowestCommonAncestor(root, Integer.parseInt(lineArray[0]),Integer.parseInt(lineArray[1])));
                     
                 }
             }
         } catch (IOException e) {
             System.out.println("File Read Error: " + e.getMessage());
         }
-	}
+    }
 }
-
